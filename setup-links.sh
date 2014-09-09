08f1628588674e426801a346b439fbd337e6d92f
Josenivaldo Benito Jr <jrbenito@benito.qsl.br>
2014-09-09 16:02:18 -0300

    Check before create .vim directory to avoid create another link inside. Also backup .bashrc

--- setup-links.sh
+++ setup-links.sh
@@ -9,9 +9,12 @@ _sed() {
 	sed -E "$1" $2 > $2.tmp && mv $2.tmp $2
 }
 
-_sed "/^additional_files=/s#\"[^\"]*\"#\"$PWD\"#g" $PWD/bash/bashrc
+#_sed "/^additional_files=/s#\"[^\"]*\"#\"$PWD\"#g" $PWD/bash/bashrc
 
 # Bash files
+if [ -f  ~/.bashrc ]; then
+    mv ~/.bashrc ~/.bashrc_backup
+fi;
 ln -sv $PWD/bash/bashrc ~/.bashrc
 ln -sv $PWD/bash/bash_profile ~/.bash_profile
 ln -sv $PWD/bash/bash_logout ~/.bash_logout
@@ -29,18 +32,18 @@ ln -sv $PWD/bash/inputrc ~/.inputrc
 # editrc. For programs that use editline library
 ln -sv $PWD/bash/editrc ~/.editrc
 
-# ackrc. For ack
-#ln -sv $PWD/bash/ackrc ~/.ackrc
-
-# Octave startup file
-#ln -sv $PWD/octave/octaverc ~/.octaverc
 
 # git files
 ln -sv $PWD/git/gitconfig ~/.gitconfig       # You might have to adjust some paths in this file
 
 # vim files
 ln -sv $PWD/vim/vimrc ~/.vimrc
-ln -sv $PWD/vim/vim ~/.vim
+if [[ -d ~/.vim && ! -h ~/.vim ]]; then
+    mv ~/.vim ~/.vim_backup
+fi
+[ ! -h ~/.vim ] && ln -sv $PWD/vim/vim ~/.vim || echo "Already exist"
+
+
 
 # Install vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
